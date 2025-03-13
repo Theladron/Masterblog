7234ff432b64e3176bb1ba3d7fe995298e762305
Theladron <Philwolframm@gmail.com>
2025-03-14 00:53:14 +0100

    Added Html, css and json files, updated the app to support writing new comments and deleting comments

--- app.py
+++ app.py
@@ -1,11 +1,55 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import blog_posts_storage
 
 app = Flask(__name__)
 
 
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    blog_posts = blog_posts_storage.read_comments()["data"]
+    return render_template('index.html', posts=blog_posts)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        name = request.form.get("name")
+        title = request.form.get("title")
+        content = request.form.get("content")
+        blog_posts_storage.add_comment(name, title, content)
+        return redirect(url_for('index'))
+    return render_template('add.html')
+
+
+@app.route('/delete/<int:post_id>')
+def delete(post_id):
+    blog_posts_storage.delete_comment(post_id)
+    return redirect(url_for('index'))
+
+
+@app.route('/update/<int:post_id>', methods=['GET', 'POST'])
+def update(post_id):
+    post = ""
+    data = blog_posts_storage.read_comments()["data"]
+    for comment in data["data"]:
+        if comment["id"] == post_id:
+            post = comment.get["content"]
+    if not post:
+        # Post not found
+        return "Post not found", 404
+
+    if request.method == 'POST':
+        for index, comment in enumerate(data["data"]):
+            if comment["id"] == post_id:
+                data["data"][index]["content"] = "abc"
+                blog_posts_storage.update_comment()
+                return redirect(url_for('index'))
+    # Update the post in the JSON file
+    # Redirect back to index
+
+    # Else, it's a GET request
+    # So display the update.html page
+    return render_template('update.html', post=post)
 
 
 if __name__ == '__main__':
---
+++ blog_posts_storage.py
@@ -0,0 +1,34 @@
+import json
+
+def read_comments():
+    with open("blog_posts.json", "r") as handle:
+        return json.loads(handle.read())
+
+def add_comment(author, title, content):
+    data = read_comments()
+    new_id = data["NEXT_POST_ID"]
+    data["data"].append({"id" : new_id, "author": author, "title" : title, "content" : content})
+    data["NEXT_POST_ID"] = new_id + 1
+    with open("blog_posts.json", "w") as handle:
+        handle.write(json.dumps(data, indent=4))
+
+def update_comment(update_id, author, title, content):
+    data = read_comments()
+    for index, comment in enumerate(data["data"]):
+        if comment["id"] == update_id:
+            comment["author"] = author
+            comment["title"] = title
+            comment["content"] = content
+    with open("blog_posts.json", "w") as handle:
+        handle.write(json.dumps(data, indent=4))
+
+def delete_comment(del_id):
+    data = read_comments()
+    for index, comment in enumerate(data["data"]):
+        if comment["id"] == del_id:
+            del data["data"][index]
+    with open("blog_posts.json", "w") as handle:
+        handle.write(json.dumps(data, indent=4))
+
+
+delete_comment(4)
