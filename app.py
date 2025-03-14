3b7a44dd7f26abe11dde4093586ab1794b1175a6
Theladron <Philwolframm@gmail.com>
2025-03-14 02:17:25 +0100

    Added update buttons and html, improved css visuals for every Html file

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
+
 import blog_posts_storage
 
 app = Flask(__name__)
-
 
 
 @app.route('/')
 def index():
     blog_posts = blog_posts_storage.read_comments()["data"]
     return render_template('index.html', posts=blog_posts)
+
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
@@ -29,28 +30,26 @@ def delete(post_id):
 
 @app.route('/update/<int:post_id>', methods=['GET', 'POST'])
 def update(post_id):
-    post = ""
     data = blog_posts_storage.read_comments()["data"]
-    for comment in data["data"]:
-        if comment["id"] == post_id:
-            post = comment.get["content"]
+    post = next((post for post in data if post["id"] == post_id), None)
+    print(post)
     if not post:
-        # Post not found
         return "Post not found", 404
 
     if request.method == 'POST':
-        for index, comment in enumerate(data["data"]):
-            if comment["id"] == post_id:
-                data["data"][index]["content"] = "abc"
-                blog_posts_storage.update_comment()
-                return redirect(url_for('index'))
-    # Update the post in the JSON file
-    # Redirect back to index
+        author = request.form.get('author')
+        title = request.form.get('title')
+        content = request.form.get('content')
+        blog_posts_storage.update_comment(post_id, author, title, content)
+        return redirect(url_for('index'))
 
-    # Else, it's a GET request
-    # So display the update.html page
+
     return render_template('update.html', post=post)
 
+    @app.route('/like/<int:post_id>')
+    def like(post_id):
+        blog_posts_storage.like_comment(post_id)
+        return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
--- blog_posts_storage.py
+++ blog_posts_storage.py
@@ -1,26 +1,36 @@
 import json
 
+
 def read_comments():
     with open("blog_posts.json", "r") as handle:
         return json.loads(handle.read())
 
+
 def add_comment(author, title, content):
     data = read_comments()
     new_id = data["NEXT_POST_ID"]
-    data["data"].append({"id" : new_id, "author": author, "title" : title, "content" : content})
+    data["data"].append({
+        "id": new_id,
+        "author": author,
+        "title": title,
+        "content": content,
+        "likes": 0
+    })
     data["NEXT_POST_ID"] = new_id + 1
     with open("blog_posts.json", "w") as handle:
         handle.write(json.dumps(data, indent=4))
+
 
 def update_comment(update_id, author, title, content):
     data = read_comments()
     for index, comment in enumerate(data["data"]):
         if comment["id"] == update_id:
-            comment["author"] = author
-            comment["title"] = title
-            comment["content"] = content
+            data["data"][index]["author"] = author
+            data["data"][index]["title"] = title
+            data["data"][index]["content"] = content
     with open("blog_posts.json", "w") as handle:
         handle.write(json.dumps(data, indent=4))
+
 
 def delete_comment(del_id):
     data = read_comments()
@@ -30,5 +40,10 @@ def delete_comment(del_id):
     with open("blog_posts.json", "w") as handle:
         handle.write(json.dumps(data, indent=4))
 
-
-delete_comment(4)
+def like_comment(post_id):
+    data = read_comments()
+    for index, comment in enumerate(data["data"]):
+        if comment["id"] == post_id:
+            data["data"][index]["likes"] += 1
+    with open("blog_posts.json", "w") as handle:
+        handle.write(json.dumps(data, indent=4))
